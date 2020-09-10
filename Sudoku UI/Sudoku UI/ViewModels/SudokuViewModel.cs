1dbfc4f05c2d1a633471b43f379326e394a1aeac
Michael Fullom <mfullom@ahfc.us>
2020-09-09 22:35:35 -0800

    inits sudoku ui

---
+++ Sudoku UI/Sudoku UI/ViewModels/SudokuViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Sudoku_Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sudoku_UI.ViewModels
+{
+	public class SudokuViewModel : BaseViewModel
+    {
+        Sudoku sudoku = null;
+        public bool showLabel { get => sudoku != null; }
+        private string boardDisplay;
+
+        public string BoardDisplay
+        {
+            get => boardDisplay;
+            set => SetProperty(ref boardDisplay, value);
+        }
+        public SudokuViewModel()
+        {
+            StartCommand = new Command(async() =>
+            {
+                sudoku = new Sudoku();
+                await ShowBoard();
+            });
+        }
+
+        public Command StartCommand { get; }
+
+        public async Task ShowBoard()
+        {
+            BoardDisplay = "Working...";
+
+            await sudoku.Init();
+
+            string _boardDisplay = "";
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    var value = sudoku.PuzzleBoard[i, j];
+                    _boardDisplay += value.HasValue ? $"| {value} " : "|    ";
+                }
+                _boardDisplay += $"|{Environment.NewLine}";
+            }
+
+            BoardDisplay = _boardDisplay;
+        }
+    }
+}
--- Sudoku UI/Sudoku UI/Views/Sudoku.xaml.cs
+++ Sudoku UI/Sudoku UI/Views/Sudoku.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sudoku_UI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace Sudoku_UI.Views
         public Sudoku()
         {
             InitializeComponent();
+            BindingContext = new SudokuViewModel();
         }
     }
 }
