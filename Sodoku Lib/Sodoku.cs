cd05dcd3cfb4f8a56db929db75e793859a9f88ab
Michael Fullom <mfullom@ahfc.us>
2020-09-09 20:07:57 -0800

    Adds sudoku page

--- Sodoku Lib/Sodoku.cs
+++ Sodoku Lib/Sodoku.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Diagnostics;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace Sodoku_Lib
--- Sodoku UI/Sodoku UI/AppShell.xaml.cs
+++ Sodoku UI/Sodoku UI/AppShell.xaml.cs
@@ -13,6 +13,7 @@ namespace Sodoku_UI
             InitializeComponent();
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(Sodoku), typeof(Sodoku));
         }
 
     }
