72f7b9e9b75b3c7961659a02bc27f272cbb66436
Michael Fullom <mfullom@ahfc.us>
2020-10-04 13:40:00 -0800

    Image shuffle

--- Sudoku UI/Sudoku UI.Android/Resources/Resource.designer.cs
+++ Sudoku UI/Sudoku UI.Android/Resources/Resource.designer.cs
@@ -8702,17 +8702,17 @@ namespace SudokuUI.Droid
 			// aapt resource value: 0x7F07006B
 			public const int design_snackbar_background = 2131165291;
 			
+			// aapt resource value: 0x7F07006F
+			public const int icons8_play_32 = 2131165295;
+			
 			// aapt resource value: 0x7F07006C
-			public const int game = 2131165292;
+			public const int ic_mtrl_chip_checked_black = 2131165292;
 			
 			// aapt resource value: 0x7F07006D
-			public const int ic_mtrl_chip_checked_black = 2131165293;
+			public const int ic_mtrl_chip_checked_circle = 2131165293;
 			
 			// aapt resource value: 0x7F07006E
-			public const int ic_mtrl_chip_checked_circle = 2131165294;
-			
-			// aapt resource value: 0x7F07006F
-			public const int ic_mtrl_chip_close_circle = 2131165295;
+			public const int ic_mtrl_chip_close_circle = 2131165294;
 			
 			// aapt resource value: 0x7F070070
 			public const int mtrl_snackbar_background = 2131165296;
@@ -8760,19 +8760,19 @@ namespace SudokuUI.Droid
 			public const int notify_panel_notification_icon_bg = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int tab_about = 2131165311;
+			public const int sudoku = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int tab_feed = 2131165312;
+			public const int tab_about = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int tooltip_frame_dark = 2131165313;
+			public const int tab_feed = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int tooltip_frame_light = 2131165314;
+			public const int tooltip_frame_dark = 2131165314;
 			
 			// aapt resource value: 0x7F070083
-			public const int xamarin_logo = 2131165315;
+			public const int tooltip_frame_light = 2131165315;
 			
 			static Drawable()
 			{
@@ -9889,14 +9889,14 @@ namespace SudokuUI.Droid
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7F0C0000
-			public const int icon = 2131492864;
-			
 			// aapt resource value: 0x7F0C0001
-			public const int icon_round = 2131492865;
+			public const int icon = 2131492865;
 			
 			// aapt resource value: 0x7F0C0002
-			public const int launcher_foreground = 2131492866;
+			public const int icon_round = 2131492866;
+			
+			// aapt resource value: 0x7F0C0000
+			public const int ic_launcher = 2131492864;
 			
 			static Mipmap()
 			{
